a60f7d7ce68684a3cf5374b474bb709bb4438e6a
José Lopes <joselopessobrinhoneto9@gmail.com>
2022-08-17 11:29:15 -0300

    HelloWorld

--- src/main/java/com/neto/crud_api/CrudApiApplication.java
+++ src/main/java/com/neto/crud_api/CrudApiApplication.java
@@ -1,4 +1,4 @@
-package com.jose.crud_api;
+package com.neto.crud_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/neto/crud_api/CrudApiApplicationTests.java
+++ src/test/java/com/neto/crud_api/CrudApiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.jose.crud_api;
+package com.neto.crud_api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
