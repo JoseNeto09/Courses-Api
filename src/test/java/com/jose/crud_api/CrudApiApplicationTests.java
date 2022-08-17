ec62347f00e626f05a4c24945ec1672ac4d326d2
José Lopes <joselopessobrinhoneto9@gmail.com>
2022-08-17 09:51:17 -0300

    first commit

---
+++ src/test/java/com/jose/crud_api/CrudApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jose.crud_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
