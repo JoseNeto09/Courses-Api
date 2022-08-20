c1ed19ae90d0d9725ecfa1e8a9977718bd843e28
José Lopes <joselopessobrinhoneto9@gmail.com>
2022-08-20 13:26:23 -0300

    Finalizando config do banco de dados, testando comunicação com angular

--- src/main/java/com/neto/crud_api/controller/CourseController.java
+++ src/main/java/com/neto/crud_api/controller/CourseController.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -19,9 +20,8 @@ public class CourseController {
     
     private final CourseRepository courseRepository; 
 
-
     @GetMapping
-    public List<Course> list() {
+    public @ResponseBody List<Course> list() {
         return courseRepository.findAll();
 
     }
