c6a813d1a117a359355fd5681d73ef1cfe66719f
José Lopes <joselopessobrinhoneto9@gmail.com>
2022-08-30 09:55:03 -0300

    GetMapping, consulta por id

--- src/main/java/com/neto/crud_api/controller/CourseController.java
+++ src/main/java/com/neto/crud_api/controller/CourseController.java
@@ -7,15 +7,15 @@ import lombok.AllArgsConstructor;
 
 import java.util.List;
 
-import javax.xml.ws.Response;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -31,11 +31,19 @@ public class CourseController {
         return courseRepository.findAll();
 
     }
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<Course> findById(@PathVariable Long id){
+        return courseRepository.findById(id)
+        .map(record -> ResponseEntity.ok().body(record))
+        .orElse(ResponseEntity.notFound().build());
+    }
+
 
     @PostMapping
-    public ResponseEntity<Course> create(@RequestBody Course course) {
-        return ResponseEntity.status(HttpStatus.CREATED)
-            .body(courseRepository.save(course));
-    } 
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create(@RequestBody Course course) {
+        return courseRepository.save(course);
+    }
 
 }
