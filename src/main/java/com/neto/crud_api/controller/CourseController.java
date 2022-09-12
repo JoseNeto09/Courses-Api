96fdfddd77c7c6269e76a2072fe466c8cd335f58
José Lopes <joselopessobrinhoneto9@gmail.com>
2022-09-11 22:22:59 -0300

    função de delete em desenvolvimento

--- src/main/java/com/neto/crud_api/controller/CourseController.java
+++ src/main/java/com/neto/crud_api/controller/CourseController.java
@@ -6,17 +6,22 @@ import com.neto.crud_api.model.*;
 import com.neto.crud_api.repository.CourseRepository;
 
 import lombok.AllArgsConstructor;
+import net.bytebuddy.dynamic.scaffold.TypeWriter.FieldPool.Record;
 
 import java.util.List;
 
+import org.apache.catalina.startup.ClassLoaderFactory.Repository;
+import org.hibernate.sql.Delete;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -58,5 +63,10 @@ public class CourseController {
                 return ResponseEntity.ok().body(updated);
              })
             .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable long id) {
+        courseRepository.deleteById(id);
     }
 }
